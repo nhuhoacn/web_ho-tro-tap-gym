0855ed64e5e5233de54799b3b6e4152a8b5c041e
Nhu Hoa <nhuhoacn@gmail.com>
2023-02-17 16:57:03 +0700

    hien thi blog theo topic

--- src/app/controllers/BlogController.js
+++ src/app/controllers/BlogController.js
@@ -33,31 +33,27 @@ class BlogControlller {
                 resolve(all_topic);
             });
         });
-        // var promises_blog_topic = new Promise((resolve, reject) => {
-        //     db.query(blog_topic, req.body.topic_id, function (err, blog) {
-        //         resolve(blog);
-        //     });
-        // });
-        const makeblog = async () => {
+        const showblog = async () => {
             try {
                 var numPage = await promises_num;
                 var blog = await promises_blog;
                 var topic = await promises_all_topic;
                 var offset = (req.params.page - 1) * numPerPage;
+                var pagenext = Number(req.params.page) + 1;
                 console.log('số page ', numPage);
                 console.log('hiển thị từ hàng thứ : ', offset);
                 console.log('topic : ', topic);
                 res.render('blog', {
-                    session: req.session,
                     blog: blog,
                     topic: topic,
                     numPage: numPage,
+                    pagenext: pagenext,
                 });
             } catch (err) {
                 console.log(err);
             }
         };
-        makeblog();
+        showblog();
     }
     //[GET] /blog/:slug
     show(req, res) {
@@ -65,7 +61,6 @@ class BlogControlller {
         db.query(search_blog, req.params.id, function (err, data) {
             if (data) {
                 res.render('detail_blog', {
-                    session: req.session,
                     blog: data[0],
                 });
             } else {
@@ -76,9 +71,7 @@ class BlogControlller {
 
     //[GET] blog/create
     create(req, res) {
-        res.render('create_blog', {
-            session: req.session,
-        });
+        res.render('create_blog');
     }
     //[POST] blog/create
     insert_blog(req, res) {
@@ -98,12 +91,78 @@ class BlogControlller {
                 console.log('blog created');
             }
         });
-        res.render('create_blog', { session: req.session });
+        res.render('create_blog');
     }
 
     //[GET] /blog/topic/:topic_id
     blog_topic(req, res) {
-        const blog_topic = 'select *from blog where topic_id = topic_id';
+        var numPerPage = 4;
+        const all_blog_topic = 'select *from blog where topic_id = ?';
+        const search_blog = `select *from blog  where topic_id= ? LIMIT ? OFFSET ?`;
+        const all_topic = `SELECT topic.topic_id,topic.name, COUNT(*) as count FROM topic right JOIN blog ON blog.topic_id = topic.topic_id GROUP BY topic.topic_id`;
+
+        // đếm số page
+        var promises_num = new Promise((resolve, reject) => {
+            db.query(
+                all_blog_topic,
+                req.params.topic,
+                function (err, all_blog) {
+                    var numPage = Math.ceil(all_blog.length / numPerPage);
+                    if (err) {
+                        console.log(err);
+                    }
+                    resolve(numPage);
+                },
+            );
+        });
+        //tìm kiếm blog sẽ hiển thị
+        var promises_blog = new Promise((resolve, reject) => {
+            var offset = (req.params.page - 1) * numPerPage;
+            var topic_id = Number(req.params.topic);
+            db.query(
+                search_blog,
+                [topic_id, numPerPage, offset],
+                function (err, blog) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    resolve(blog);
+                },
+            );
+        });
+
+        // đếm xem mỗi loại topic có bao nhiêu bài
+        var promises_all_topic = new Promise((resolve, reject) => {
+            db.query(all_topic, function (err, all_topic) {
+                if (err) {
+                    console.log(err);
+                }
+                resolve(all_topic);
+            });
+        });
+        const showblog = async () => {
+            try {
+                var numPage = await promises_num;
+                var blog = await promises_blog;
+                var topic = await promises_all_topic;
+                var offset = (req.params.page - 1) * numPerPage;
+                var pagenext = Number(req.params.page) + 1;
+                console.log('số page ', numPage);
+                console.log('hiển thị từ hàng thứ : ', offset);
+                console.log('topic : ', req.params.page);
+                console.log('blog : ', blog);
+                res.render('blog', {
+                    blog: blog,
+                    topic: topic,
+                    page_topic: req.params.topic,
+                    numPage: numPage,
+                    pagenext: pagenext,
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        showblog();
     }
 }
 
